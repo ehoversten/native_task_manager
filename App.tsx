9a30f7e6fd39f8bc7187d98a2772569c96651599
Erik Hoversten <ehoversten@gmail.com>
2024-07-11 20:36:16 -0400

    initial setup

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Who is your daddy and what does he do?</Text>
       <StatusBar style="auto" />
     </View>
   );
