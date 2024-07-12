d5aab843c3370442ec8ee36fb1b1e8f90089b0b4
Erik Hoversten <ehoversten@gmail.com>
2024-07-11 23:14:03 -0400

    firebase auth functionality working

--- App.tsx
+++ App.tsx
@@ -1,34 +1,88 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { User, onAuthStateChanged } from 'firebase/auth';
 import Home from './src/screens/Home';
 import Login from './src/screens/Login';
+import Welcome from './src/screens/Welcome';
+import TaskList from './src/screens/TaskList';
+import { FIREBASE_AUTH } from './FirebaseConfig';
 
 export type RootStackParamList = {
   Home: undefined;
   Login: undefined;
+  Welcome: undefined;
+  TaskList: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name='Home'
-          component={Home}
-          options={{
-            title: "Task Manager"
-          }}/>
-        <Stack.Screen 
-          name='Login'
-          component={Login}
-          options={{
-            title: "Login"
-          }}/>
+const InsideStack = createNativeStackNavigator();
 
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen name="Welcome" component={Welcome}/>
+      <InsideStack.Screen name="TaskList" component={TaskList}/>
+    </InsideStack.Navigator>
+  )
+}
+
+export default function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log('User: ', user);
+      setUser(user);
+    })
+  }, [])
+
+  return (
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen 
+    //       name='Home'
+    //       component={Home}
+    //       options={{
+    //         title: "Task Manager"
+    //       }}/>
+    //     { user ? (
+    //       <Stack.Screen 
+    //       name='Welcome'
+    //       component={Welcome}
+    //       options={{
+    //         title: "Welcome"
+    //       }}/>
+    //       ) : (
+    //         <Stack.Screen 
+    //           name='Login'
+    //           component={Login}
+    //           options={{
+    //             title: "Login"
+    //           }}/>
+    //     )}
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        { user ? (
+          <Stack.Screen 
+          name='InsideLayout'
+          component={InsideLayout}
+          options={{
+            headerShown: false
+          }}/>
+          ) : (
+            <Stack.Screen 
+              name='Login'
+              component={Login}
+              options={{
+                title: "Login"
+              }}/>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/Welcome.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../../App'
+import { FIREBASE_AUTH } from '../../FirebaseConfig'
+
+type WelcomeProps = NativeStackScreenProps<RootStackParamList, 'Welcome'>
+
+export default function Welcome({navigation}: WelcomeProps) {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome</Text>
+      <Button 
+        title='TaskList'
+        onPress={() => navigation.navigate('TaskList')} />
+      <Button 
+        title='Logout'
+        onPress={() => FIREBASE_AUTH.signOut()} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
